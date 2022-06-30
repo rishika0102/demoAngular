48a845bb322e0776426f113d312be10950d9b5c5
rishika0102 <rishikaa@shriffle.com>
2022-06-30 17:40:38 +0530

    all binding acheiving code with understanding and implementation

--- demoAngularApp/src/app/test/test.component.ts
+++ demoAngularApp/src/app/test/test.component.ts
@@ -9,6 +9,10 @@ export class TestComponent implements OnInit {
 
   public name = "Jhon";
   public siteUrl = window.location.href;
+  public greeting = "";
+  public greets = "";
+  public isDisable = true;
+  public myId = "testId"
   constructor() { }
 
   ngOnInit(): void {
@@ -17,5 +21,8 @@ export class TestComponent implements OnInit {
   userName() {
     return "returning data by method calling"+" "+this.name;
   }
-
+  onClick() {
+    console.log("Welcome! This is event binding");
+    this.greeting = 'Welcome! This is Event binding'
+  }
 }
